6f73fcd87d4d0490d7a75337e0591cd8e0faede8
Kim <tjdqhdrla92@gmail.com>
2018-03-06 09:56:21 +0900

    봉성 Cart 진행중

---
+++ src/main/java/vo/CartVo.java
@@ -0,0 +1,43 @@
+package vo;
+
+public class CartVo {
+	int car_no, p_no, cart_amt;
+	String m_id, cart_regdate;
+	
+	public CartVo() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public int getCar_no() {
+		return car_no;
+	}
+	public void setCar_no(int car_no) {
+		this.car_no = car_no;
+	}
+	public int getP_no() {
+		return p_no;
+	}
+	public void setP_no(int p_no) {
+		this.p_no = p_no;
+	}
+	public int getCart_amt() {
+		return cart_amt;
+	}
+	public void setCart_amt(int cart_amt) {
+		this.cart_amt = cart_amt;
+	}
+	public String getM_id() {
+		return m_id;
+	}
+	public void setM_id(String m_id) {
+		this.m_id = m_id;
+	}
+	public String getCart_regdate() {
+		return cart_regdate;
+	}
+	public void setCart_regdate(String cart_regdate) {
+		this.cart_regdate = cart_regdate;
+	}
+	
+	
+}
